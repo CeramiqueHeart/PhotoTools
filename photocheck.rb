706c6ce7c57ff67d34b0afb6029c42bc8556653e
CeramiqueHeart <CeramiqueHeart@users.noreply.github.com>
2018-08-17 17:44:29 +0900

    Improve movie file identity check method

--- photocheck.rb
+++ photocheck.rb
@@ -1,6 +1,10 @@
-require 'exifr'
+require 'fileutils'
+require 'exifr/jpeg'
+require 'streamio-ffmpeg'
 
-file_type = ["jpg", "jpeg", "mov", "mp4"]
+file_type    = ["jpg", "jpeg", "mov", "mp4"]
+exist_folder = "exists"
+exist_files  = Array.new
 
 file_type.map! do |item|
   item = "**/*." + item
@@ -12,20 +16,31 @@ File.open("photoscan.db", "rb") do |file|
 end
 
 Dir.chdir(ARGV[0]) do
-  Dir.glob(file_type, File::FNM_CASEFOLD) do |file|
+  Dir.glob(file_type, File::FNM_CASEFOLD).sort.each do |file|
     if data.has_key?(File.basename(file))
       if File.extname(file).downcase == '.jpg' or
          File.extname(file).downcase == '.jpeg'
         unless data[File.basename(file)].index(EXIFR::JPEG.new(file).date_time_original)
           puts file + ': ' + 'not found'
+        else
+          exist_files.push(file)
         end
       else
-        unless data[File.basename(file)].index(File.mtime(file))
+        unless data[File.basename(file)].index(FFMPEG::Movie.new(file).creation_time)
           puts file + ': ' + 'not found'
+        else
+          exist_files.push(file)
         end
       end
     else
       puts file + ': ' + 'not found'
     end
   end
+
+  puts "Moving existing files to '#{exist_folder}'"
+  unless Dir.exists?(exist_folder) then
+    Dir.mkdir(exist_folder)
+  end
+  FileUtils.mv(exist_files, exist_folder)
 end
+
