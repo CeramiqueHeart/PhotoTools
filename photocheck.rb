910290c0cb5131cc099ece0f33ce2bfde9b2c238
CeramiqueHeart <CeramiqueHeart@users.noreply.github.com>
2018-12-24 12:19:55 +0900

    Introduced file attiribution cache (only scan unknown files)

--- photocheck.rb
+++ photocheck.rb
@@ -5,28 +5,30 @@ require 'streamio-ffmpeg'
 file_type    = ["jpg", "jpeg", "mov", "mp4"]
 exist_folder = "exists"
 exist_files  = Array.new
+file_list    = nil
+file_data    = nil
+db_file      = "photoscan.db"
 
 file_type.map! do |item|
   item = "**/*." + item
 end
 
-data = nil
-File.open("photoscan.db", "rb") do |file|
-  data = Marshal.load(file)
+File.open(db_file, "rb") do |file|
+  file_list, file_data = Marshal.load(file)
 end
 
 Dir.chdir(ARGV[0]) do
   Dir.glob(file_type, File::FNM_CASEFOLD).sort.each do |file|
-    if data.has_key?(File.basename(file))
+    if file_data.has_key?(File.basename(file))
       if File.extname(file).downcase == '.jpg' or
          File.extname(file).downcase == '.jpeg'
-        unless data[File.basename(file)].index(EXIFR::JPEG.new(file).date_time_original)
+        unless file_data[File.basename(file)].index(EXIFR::JPEG.new(file).date_time_original)
           puts file + ': ' + 'not found'
         else
           exist_files.push(file)
         end
       else
-        unless data[File.basename(file)].index(FFMPEG::Movie.new(file).creation_time)
+        unless file_data[File.basename(file)].index(FFMPEG::Movie.new(file).creation_time)
           puts file + ': ' + 'not found'
         else
           exist_files.push(file)
--- photoscan.rb
+++ photoscan.rb
@@ -2,27 +2,38 @@ require 'exifr/jpeg'
 require 'streamio-ffmpeg'
 
 file_type = ["jpg", "jpeg", "mov", "mp4"]
-data      = Hash.new
+file_list = Array.new
+file_data = Hash.new
+db_file   = "photoscan.db"
 
 file_type.map! do |item|
   item = "**/*." + item
 end
 
+if (File.exist? db_file)
+  File.open(db_file, "rb") do |file|
+    file_list, file_data = Marshal.load(file)
+  end
+end
+
 Dir.chdir(ARGV[0]) do
   Dir.glob(file_type, File::FNM_CASEFOLD) do |file|
-    puts file
-    unless data.has_key?(File.basename(file))
-        data[File.basename(file)] = Array.new
-    end
-    if File.extname(file).downcase == '.jpg' or
-       File.extname(file).downcase == '.jpeg'
-      data[File.basename(file)].push(EXIFR::JPEG.new(file).date_time_original)
-    else
-      data[File.basename(file)].push(FFMPEG::Movie.new(file).creation_time)
+    unless file_list.include? file
+      puts file
+      file_list.push file
+      unless file_data.has_key?(File.basename(file))
+          file_data[File.basename(file)] = Array.new
+      end
+      if File.extname(file).downcase == '.jpg' or
+        File.extname(file).downcase == '.jpeg'
+        file_data[File.basename(file)].push(EXIFR::JPEG.new(file).date_time_original)
+      else
+        file_data[File.basename(file)].push(FFMPEG::Movie.new(file).creation_time)
+      end
     end
   end
 end
 
-File.open("photoscan.db", "wb") do |file|
-    Marshal.dump(data, file)
+File.open(db_file, "wb") do |file|
+    Marshal.dump([file_list, file_data], file)
 end
